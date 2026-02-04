f4933baf90ebe850aa884e7bb6fbeb27bde9cc4f
crpetrie67 <cpe4735@stacmail.net>
2026-02-05 12:34:35 +1300

    intro to python basics

--- Python Main.py
+++ Python Main.py
@@ -4,3 +4,21 @@ print(123)
 
 # when printing text, it must be in speechmarks which turns it into a string
 print("Hello")
+
+# There are lots of different data types:
+# integers, decimalds (floating point numbers)
+# text (string), boolean (true or false)
+
+# We use variables to store information
+name = "Pluto"
+First_name = "John"
+last_name = "Smith"
+age = 14
+
+# You can include variables inside print() statements
+print(name)
+# To combine variables with a string, we use f-strings
+# The variable goes inside curly brackets{}
+print(f"my dog is called {name} and his is {age} years old")
+
+# We use input() to get input from the user
